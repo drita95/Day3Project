e97dba8f76f1cf31f05f20f4699ec879d50417ab
Drita Alla <dritaalla3@gmail.com>
2020-10-06 21:59:30 +0200

    added new files

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,4 +4,11 @@ public class AwesomeTest {
     //green means it's under staging area
     //grey in.idea folder means it's ignored
     //when the color is normal means it's commited no changes
+
+    // I just pull the new changes from GiHub that i did not have locally
+    //and now my project is in sync with remote repository
+
+    //Now i made more awesome changes and I am about to push this back to GitHub
+
 }
+
